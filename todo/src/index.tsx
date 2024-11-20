7ff842fbb62e18041ee21f325ca79b7999dd4dbe
yasmeenq <yasmeen.qawasmy@gmail.com>
2024-11-20 16:57:52 +0200

    changes

--- todo/src/index.tsx
+++ todo/src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { Task } from './components/Task/Task';
 import { TaskList } from './components/TaskList/TaskList';
+    
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
